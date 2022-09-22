17983b629ef1e44da21fa4d5c96f3c3983b111f5
tialedencan <tialedencan@gmail.com>
2022-09-22 21:17:00 +0200

    App with local notification

--- LocalNotification/app/src/main/java/com/tia/localnotification/MainActivity.kt
+++ LocalNotification/app/src/main/java/com/tia/localnotification/MainActivity.kt
@@ -1,23 +1,32 @@
 package com.tia.localnotification
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.annotation.RequiresApi
+import com.jakewharton.threetenabp.AndroidThreeTen
 import kotlinx.android.synthetic.main.activity_main.*
 import org.w3c.dom.Text
 import java.lang.reflect.Modifier
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AndroidThreeTen.init(this)
 
         val service = CounterNotificationService(applicationContext)
         btnShowNotification.setOnClickListener() {
             service.showNotification(Counter.value)
         }
-
+        val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy.  HH:mm")
+        val time=LocalDateTime.now().format(formatter)
+        editTextTime.setText(time.toString())
     }
 }
 
